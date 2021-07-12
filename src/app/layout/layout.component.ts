8e3e2681c22bbd42c2fcaa07779daba3ce55d2cb
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-12 18:50:25 -0400

    Creado contenido del layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  level = 1;
+  enableNotTable = false;
 
-  constructor() { }
+  registers = [
+    {
+      title: 'One',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'fa-user',
+    },
+    {
+      title: 'Two',
+      content:
+        'Ut maximus tincidunt nulla sit amet tempus. Ut eget interdum lacus.',
+      icon: 'fa-plus',
+    },
+    {
+      title: 'Three',
+      content: 'Vestibulum ac luctus ipsum..',
+      icon: 'fa-minus',
+    },
+    {
+      title: 'Four',
+      content:
+        'Nam hendrerit mattis nisi a tempor. In pharetra felis quis magna accumsan malesuada. Suspendisse tristique purus quam, id auctor enim molestie ut.',
+      icon: 'fa-angle-up',
+    },
+    {
+      title: 'Five',
+      content:
+        'Proin non odio id nisl fringilla vestibulum in vehicula nibh. Sed vel tincidunt eros.',
+      icon: 'fa-angle-down'
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void { }
+
+  getLabel(): string {
+    return `level-${this.level}`;
   }
-
 }
+
